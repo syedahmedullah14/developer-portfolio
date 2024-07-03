505ed84f60e05e667278fe984bd658c52907033a
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-03 20:50:47 +0530

    building the Grid component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
       </div>
 
