1515674591b23e132406d8c04815e75eede1ee32
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-03 19:00:55 +0530

    Installed framer motion with acerternity UI

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>
+        <div className='pb-20 pt-36'>
+             
+        </div>
+    </div>
   )
 }
 
