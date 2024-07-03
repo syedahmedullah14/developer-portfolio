5b35f978343e599eb497c89fd357007dc2f23d37
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-03 20:19:24 +0530

    modified TextGenerateEffect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
-import { TextGenerateEffect, TextGenerateEffectDemo } from './ui/TextGenerateEffect'
+import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
 const Hero = () => {
   return (
@@ -24,10 +24,16 @@ const Hero = () => {
                 <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
                     Dynamic Web Magic with Next.js
                 </p>
+
                 <TextGenerateEffect 
                     words="Transforming Concepts into Seamless User Experiences"
                     className="text-center text-[40px] md:text-5xl lg:text-6xl"
                 />
+
+                <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+                    Hi, I&apos;m Shareef, A Data scientist based in India. 
+                </p>
+
             </div>
         </div>
 
