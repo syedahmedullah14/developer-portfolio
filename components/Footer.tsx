749a608e52a51b33b6557b3880482bddc08753b3
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-05 01:46:37 +0530

    Updated BentoGrid

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -42,7 +42,7 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-4"
             >
               <a href={info.link}>
                 <img src={info.img} alt="icons" width={20} height={20} />
--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -51,7 +51,7 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
-  const leftLists = ["Gen AI", "Machine Learning", "Deep learning"];
+  const leftLists = ["Gen AI", "ML", "Deep learning"];
   const rightLists = ["NLP", "MLOPs", "LLMOps"];
 
   const [copied, setCopied] = useState(false);
