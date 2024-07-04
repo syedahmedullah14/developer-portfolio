842442c6ac7997f1df800d384f512052c23db233
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-04 19:28:21 +0530

    updated layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Shareef's's Portfolio",
+  title: "Shareef's Portfolio",
   description: "Modern & Minimal Portfolio",
 };
 
