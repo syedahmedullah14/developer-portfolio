eb0e722379ad8479ef9f7f9eb4e6daaa905d453f
Ahmedullah Jaser Syed <syedahmedullahjaser@gmail.com>
2024-07-05 09:15:24 +0530

    Updated Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -34,7 +34,7 @@ const Footer = () => {
         </a>
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
-        <p className="md:text-base text-sm md:font-normal font-light">
+        <p className="md:text-base text-sm md:font-normal font-light mb-2">
           Copyright © 2024 Shareef Shahzer
         </p>
 
@@ -42,7 +42,7 @@ const Footer = () => {
           {socialMedia.map((info) => (
             <div
               key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-4"
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300 mt-4 mb-8"
             >
               <a href={info.link}>
                 <img src={info.img} alt="icons" width={20} height={20} />
